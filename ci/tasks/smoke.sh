57a7bff68b2612af4088198b1f1135863f233a97
tkaburagi <tkaburagi@kabu-2.local>
2016-10-15 11:11:57 +0900

    test

--- ci/tasks/smoke.sh
+++ ci/tasks/smoke.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
-echo "hello";
+sleep 10
+
+i=0
+
+while [[ $i -ne 10 ]]; do
+	code=`curl -LI http://127.0.0.1:8080/ -o /dev/null -w '%{http_code}\n' -s`;
+	if [[ ${code} = "200" ]]; then
+	    echo ${code};
+		echo "ok";
+	else
+		echo "abort!!";
+		exit 1
+	fi
+	i=`expr $i + 1`;
+	sleep 1;
+done
